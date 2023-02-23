fd59ff2f292afb6279e3cafde033226d61865fe9
Sam Gillies <sam@samgillies.com>
2023-02-23 20:21:01 +0000

    umenus position correctly on presentation view

--- createRecordingChain.js
+++ createRecordingChain.js
@@ -12,12 +12,14 @@ var recEnable = this.patcher.getnamed('recEnable');
 var timeElapsed = this.patcher.getnamed('timeElapsed'); 
 var numInputs = 0; 
 var a = 0; 
+var pos = 20; 
 
 function createInputs(a) { 
 
     if (a < 1) a = 1;
     if (a > 16) a = 16; 
 
+    // remove existing objects
     if (numInputs) {
         this.patcher.remove(therecorder);
 
@@ -32,19 +34,26 @@ function createInputs(a) {
     }
 
     numInputs = a; 
-
+    // create new objects
     if (numInputs) {
         therecorder = this.patcher.newdefault(300, 300, "sfrecord~", a);
+        inputslist = this.patcher.newdefault(20, -50, "receive", "audioInputs");
 
         for (var i = 0; i < a; i++) {
-                inputslist[i] = this.patcher.newdefault(300+(i * 150), 50, "receive", "audioInputs");
-                umenus[i] = this.patcher.newdefault(300+(i * 150), 100, "umenu");
-                setters[i] = this.patcher.newdefault(300+(i * 150), 150, "prepend", "set");
-                receivers[i] = this.patcher.newdefault(300+(i * 150), 200, "receive~");
+                if ((i >= 0) && (i <= 3)) { pos = 20 }; 
+                if ((i >= 4) && (i <= 7)) { pos = 50 };
+                if ((i >= 8) && (i <= 11)) { pos = 80 };
+                if ((i >= 12) && (i <= 15)) { pos = 110 };
 
-                umenus[i].presentation(1); 
+                umenus[i] = this.patcher.newdefault(20+((i % 4) * 110), pos, "umenu");
 
-                this.patcher.connect(inputslist[i], 0, umenus[i], 0);
+
+                setters[i] = this.patcher.newdefault(20+(i * 150), 150, "prepend", "set");
+                receivers[i] = this.patcher.newdefault(20+(i * 150), 200, "receive~");
+
+                umenus[i].presentation(1); // add umenus to presentation mode
+
+                this.patcher.connect(inputslist, 0, umenus[i], 0);
                 this.patcher.connect(umenus[i], 0, setters[i], 0);
                 this.patcher.connect(setters[i], 0, receivers[i], 0);
                 this.patcher.connect(receivers[i], 0, therecorder, i);
